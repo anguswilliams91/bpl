bf2d9ba615b5395b6a83295afd3109e9b46cdd96
anguswilliams91 <anguswilliams91@gmail.com>
2018-09-07 18:09:08 +0100

    ...

--- src/bpl/util.py
+++ src/bpl/util.py
@@ -12,24 +12,16 @@ class ModelNotConvergedWarning(Warning):
 
 
 class suppress_output:
-    """ Suppress all stdout and stderr. """
+    """ Suppress stdout and stderr from stan. """
 
     def __init__(self):
-        """
-        self.null_fds = [
-            os.open(os.devnull, os.O_RDWR),
-            os.open(os.devnull, os.O_RDWR)
-        ]
-        """
         self.null_fds = [
             mkstemp(),
             mkstemp()
         ]
-        # Save the actual stdout (1) and stderr (2) file descriptors.
         self.save_fds = [os.dup(1), os.dup(2)]
 
     def __enter__(self):
-        # Assign the null pointers to stdout and stderr.
         os.dup2(self.null_fds[0][0], 1)
         os.dup2(self.null_fds[1][0], 2)
 
@@ -48,11 +40,9 @@ class suppress_output:
         return contents
 
     def __exit__(self, *_):
-        # Re-assign the real stdout/stderr back to (1) and (2)
         os.dup2(self.save_fds[0], 1)
         os.dup2(self.save_fds[1], 2)
 
-        # Close the null files and descriptors.
         for fd in self.save_fds:
             os.close(fd)
 
